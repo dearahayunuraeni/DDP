48a85e0ce007df1bd8511884c14b7f37d174baa8
dearahayunuraeni_SI12_0110124073 <dearahayu270@gmail.com>
2024-12-01 19:57:21 +0700

    Add files via upload

---
+++ call.py
@@ -0,0 +1,23 @@
+import luasBangunDatar
+
+luasBangunDatar.luasPersegi(6)
+luasBangunDatar.luasPersegiPanjang(20, 4)
+luasBangunDatar.luasSegitiga(4, 8)
+luasBangunDatar.luasLingkaran(15)
+luasBangunDatar.luasJajargenjang(25, 30)
+
+import luasBangunRuang
+
+luasBangunRuang.luasBalok(15, 6, 8)
+luasBangunRuang.luasKubus(8)
+luasBangunRuang.luasTabung(6, 3)
+luasBangunRuang.luasLimasSegitiga(2, 4)
+luasBangunRuang.luasPrismaSegitiga(5, 10)
+
+import hitung
+
+hitung.tambah(20, 3)
+hitung.kurang(25, 15)
+hitung.kali(10, 10)
+hitung.bagi(50, 10)
+hitung.pangkat(15,3)
