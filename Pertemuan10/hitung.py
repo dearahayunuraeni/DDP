35325e24fda10dcb8916d2f02bbda7cccf722acf
dearahayunuraeni_SI12_0110124073 <dearahayu270@gmail.com>
2024-12-19 10:22:38 +0700

    Add files via upload

---
+++ Pertemuan10/hitung.py
@@ -0,0 +1,21 @@
+import math
+
+def tambah(angka1, angka2):
+    hasil = 20 + 3
+    print("Hasil Tambah Dari",20, "+", 3, "=", hasil)
+
+def kurang(angka1, angka2):
+    hasil = 25 - 15
+    print("Hasil Pengurangan Dari",25, "-", 15, "=", hasil)
+
+def kali(angka1, angka2):
+    hasil = 10 * 10
+    print("Hasil Perkalian Dari",10, "*", 10, "=", hasil)
+
+def bagi(angka1, angka2):
+    hasil = 50 / 10
+    print("Hasil Pembagian Dari",50, "/", 10, "=", hasil)
+
+def pangkat(angka1, angka2):
+    hasil = math.pow(15, 3)
+    print("Hasil Pemangkatan Dari",15, "^", 3, "=", hasil)
